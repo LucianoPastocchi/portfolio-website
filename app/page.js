dc372ef8b65a50b18f85e520b233577999e09af0
System Administrator <root@MacBook-Air.fibertel.com.ar>
2023-06-28 19:12:46 -0300

    nav

--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
-import Image from "next/image";
-
+import { BsFillMoonStarsFill } from "react-icons/bs";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="bg-white px-10 ">
+      <section className="min-h-screen">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl font-burtons">developedby</h1>
+          <ul className="flex items-center">
+            <li>
+              <BsFillMoonStarsFill className=" cursor-pointer text-2xl" />
+            </li>
+            <li>
+              <a
+                className="bg-gradient-to-r bg-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                href=""
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </main>
   );
 }
