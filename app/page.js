c907ffe1d409965d1587fa65bb779933430ce9a0
System Administrator <root@MacBook-Air.fibertel.com.ar>
2023-06-29 18:57:49 -0300

    portfolio and dark mode

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import Image from "next/image";
@@ -5,84 +6,174 @@ import avatar from "../public/ReadyPlayerMe-Avatar.png";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
-export default function Home() {
-  return (
-    <main className="bg-white px-10 ">
-      <section className="min-h-screen">
-        <nav className="py-10 mb-12 flex justify-between">
-          <h1 className="text-xl font-burtons">developedby</h1>
-          <ul className="flex items-center">
-            <li>
-              <BsFillMoonStarsFill className=" cursor-pointer text-2xl" />
-            </li>
-            <li>
-              <a
-                className="bg-gradient-to-r bg-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                href=""
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="text-center p-10  py-10">
-          <h2 className="text-5xl py-2 text-teal-600 font-medium">
-            Luciano Pastocchi
-          </h2>
-          <h3 className="text-2xl py-2">Full Stak JAVA Developer</h3>
-          <p className="text-md py-5 leading-8 text-gray-800">
-            My name is Luciano, I am an IT student and software developer with
-            high level of motivation and quickness to pick up new skills. I
-            always try to give the best of me. I am now looking forward to
-            taking up new challenges and enhance my professional development.
-          </p>
-        </div>
-        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 cursor-pointer">
-          <AiFillLinkedin />
-          <AiFillGithub />
-        </div>
-        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-          <Image src={avatar} />
-        </div>
-      </section>
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
+import { useState } from "react";
 
-      <section>
-        <div>
-          <h3 className="text-3xl py-1">Work Experience</h3>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            <span className="text-teal-500">BBVA</span>
-          </p>
-        </div>
-        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-          <Image src={design} width={100} height={100} className="mx-auto" />
-          <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-          <p className="py-2">Creating elegant designs</p>
-          <h4 className="py-4 text-teal-600">Design tools I use</h4>
-          <p className="text-gray-800 py-1">Photoshop</p>
-          <p className="text-gray-800 py-1">Figma</p>
-        </div>
-        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-          <Image
-            src={consulting}
-            width={100}
-            height={100}
-            className="mx-auto"
-          />
-          <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-          <p className="py-2">Creating elegant designs</p>
-          <h4 className="py-4 text-teal-600">Design tools I use</h4>
-          <p className="text-gray-800 py-1">Photoshop</p>
-          <p className="text-gray-800 py-1">Figma</p>
-        </div>
-        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-          <Image src={code} width={100} height={100} className="mx-auto" />
-          <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-          <p className="py-2">Creating elegant designs</p>
-          <h4 className="py-4 text-teal-600">Design tools I use</h4>
-          <p className="text-gray-800 py-1">Photoshop</p>
-          <p className="text-gray-800 py-1">Figma</p>
-        </div>
-      </section>
-    </main>
+export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">developedby</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r bg-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href=""
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10  py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+              Luciano Pastocchi
+            </h2>
+            <h3 className="text-2xl py-2 md:text-3xl">
+              Full Stak JAVA Developer
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
+              My name is Luciano, I am an IT student and software developer with
+              high level of motivation and quickness to pick up new skills. I
+              always try to give the best of me. I am now looking forward to
+              taking up new challenges and enhance my professional development.
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 cursor-pointer">
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+            <Image src={avatar} />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Work Experience</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              <span className="text-teal-500">BBVA</span>
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                src={design}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">Creating elegant designs</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                src={consulting}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">Creating elegant designs</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={code} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">Creating elegant designs</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              My name is Luciano, I am an IT student and software developer with
+              high level of motivation and quickness to pick up new skills. I
+              always try to give the best of me. I am now looking forward to
+              taking up new challenges and enhance my professional development.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web1}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web2}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web3}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web4}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web5}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web6}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
