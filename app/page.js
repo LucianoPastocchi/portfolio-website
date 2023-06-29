decbc841bdd585b1bed1ada8fd9c4d22f4795417
System Administrator <root@MacBook-Air.fibertel.com.ar>
2023-06-29 19:16:48 -0300

    deletions

--- app/page.js
+++ app/page.js
@@ -9,9 +9,9 @@ import consulting from "../public/consulting.png";
 import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
 import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+// import web4 from "../public/web4.png";
+// import web5 from "../public/web5.png";
+// import web6 from "../public/web6.png";
 import { useState } from "react";
 
 export default function Home() {
@@ -147,7 +147,7 @@ export default function Home() {
                 height={"100%"}
               />
             </div>
-            <div className="basis-1/3 flex-1">
+            {/* <div className="basis-1/3 flex-1">
               <Image
                 src={web4}
                 className="rounded-lg object-cover"
@@ -170,7 +170,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
               />
-            </div>
+            </div> */}
           </div>
         </section>
       </main>
